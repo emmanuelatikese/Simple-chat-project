d9336cab03450066e4199e46e9f36752643c92ba
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-08-20 10:28:07 +0100

    setting up frontend and starting with authentication system

---
+++ Client/pages/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div className="min-w-80 h-72 flex flex-col bg-black">
+    <h>Login</h>
+    </div>
+  )
+}
+
+export default Login
---
+++ Client/src/App.jsx
@@ -0,0 +1,9 @@
+import Login from "../pages/Login"
+
+export default function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+    <Login/>
+    </div>
+  )
+}
