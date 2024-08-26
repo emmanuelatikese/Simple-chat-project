c610e74551deefd49b8f0e33ee8d13b1747ac0d0
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-08-26 05:02:55 +0100

    small change in the NoChat component

--- Client/src/components/ChatBox/NoChat.jsx
+++ Client/src/components/ChatBox/NoChat.jsx
@@ -1,10 +1,12 @@
 import { MdMessage } from "react-icons/md";
+import { useAuthContext } from "../../context/AuthContext";
 
 const NoChat = () => {
+    const {authUser} = useAuthContext();
   return (
     <div>
       <div className='flex justify-center w-96 flex-col items-center gap-2'>
-      <p className='font-bold text-sky-500'>Hey, Mary Doe ... 😃</p>
+      <p className='font-bold text-sky-500'>Hey, {authUser.username} ... 😃</p>
       <p className='font-bold text-sky-500'>Welcome to Simple Chat👋</p>
       <p className='font-bold text-sky-500'>Select a chat to start conversation</p>
       <MdMessage className='text-6xl text-sky-500'/>
